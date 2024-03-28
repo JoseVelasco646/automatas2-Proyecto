6cff606c98e516fa83107846786ee2025f0adc23
jose velasco <al19760627@ite.edu.mx>
2024-03-27 19:55:05 -0700

    templates actualizacion

--- sistemaa/app/core/erp/views.py
+++ sistemaa/app/core/erp/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 
 
+
 # Create your views here.
 def home(request):
-    data = {
-        'name': 'jose'
-    }
+  
 
-    return render(request, '/home/josev646/Documentos/sistemaa/app/core/erp/templates/index.html', data)
+    return render(request, '/home/josev646/Documentos/sistemaa/app/core/erp/templates/home.html')
