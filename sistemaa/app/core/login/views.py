2f28b35114a9fb62ecbb096cb672e0dd7022acd2
jose velasco <al19760627@ite.edu.mx>
2024-04-18 22:57:52 -0700

    Readme instalacion

--- sistemaa/app/core/login/views.py
+++ sistemaa/app/core/login/views.py
@@ -1,10 +1,19 @@
 from typing import Any
 from django.contrib.auth.views import LoginView
+from django.http import HttpRequest
+from django.http.response import HttpResponse as HttpResponse
+from django.shortcuts import redirect
 
 class LoginFormView(LoginView):
     template_name = 'login.html'
+
+    def dispatch(self,request,*args,**kwargs):
+        if request.user.is_authenticated:
+            return redirect('category_list')
+        return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         contex = super().get_context_data(**kwargs)
         contex['title'] = 'iniciar sesion'
         return contex
+    
